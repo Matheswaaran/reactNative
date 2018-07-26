1cbc27851f1ddf3cd16cb529ece45782d890ce1a
matheswaaran <matheswaarans@gmail.com>
2018-07-26 12:59:27 +0530

    TextInput with Button

--- textinput/App.js
+++ textinput/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import {TextInput, Text, View} from 'react-native';
+import {ToastAndroid, TextInput, Text, View, TouchableNativeFeedback, Alert} from 'react-native';
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = {text: ''};
+    this.state = {text: '', data: ''};
+  }
+
+  onPressHandler = () => {
+    // ToastAndroid.show('Converting...', ToastAndroid.SHORT);
+    this.setState({data: this.state.text.split(' ').map((x) => x && '🕷').join(' ')});
   }
 
   chageTextHandler = (e) => {
@@ -14,9 +19,14 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={{padding: 10}}>
-        <TextInput style={{height: 40}} placeholder="Type the text here" onChangeText={this.chageTextHandler}/>
+        <TextInput style={{height: 40}} placeholder="Type the text here" onChangeText={this.chageTextHandler} autoFocus/>
+        <TouchableNativeFeedback onPress={this.onPressHandler}>
+          <View style={{padding: 10, boderRadius: '5px', alignItems: 'center', backgroundColor: '#2196F3'}}>
+            <Text style={{ color: 'white' }}>Convert</Text>
+          </View>
+        </TouchableNativeFeedback>
         <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((x) => x && '🕷').join(' ')}
+          {this.state.data}
         </Text>
       </View>
     );

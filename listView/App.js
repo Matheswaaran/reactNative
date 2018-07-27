8dd7b4d1a57c45c450f64e6576592ef0002ba064
matheswaaran <matheswaarans@gmail.com>
2018-07-27 12:06:08 +0530

    SctionList

--- listView/App.js
+++ listView/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {Alert, StyleSheet, TouchableOpacity, ToastAndroid, FlatList, Text, View} from 'react-native';
+import {Alert, StyleSheet, ScrollView, TouchableOpacity, ToastAndroid, SectionList, Text, View} from 'react-native';
 
 export default class App extends React.Component {
   constructor(props){
     super(props);
-    this.state = {data: [{key: 'Devin'}, {key: 'Jackson'}, {key: 'James'}, {key: 'Joel'}, {key: 'John'}, {key: 'Jillian'}, {key: 'Jimmy'}, {key: 'Julie'}]}
+    this.state = {data: [{key: 'Devin'}, {key: 'Aadhi'}, {key: 'Jackson'}, {key: 'James'}, {key: 'Joel'}, {key: 'John'}, {key: 'Jillian'}, {key: 'Jimmy'}, {key: 'Julie'}, {key: 'Jay'}]};
   }
 
   renderItemHandler = ({item}) => {
@@ -20,12 +20,31 @@ export default class App extends React.Component {
         <Text style={styles.item}>{item.key}</Text>
       </TouchableOpacity>
     );
-  }
+  };
+
+  renderSectionsHandler = ({section}) => (
+    <Text style={styles.sectionHeader}>{section.title}</Text>
+  );
 
   render() {
+    let records = this.state.data.map(x => x);
+    let results = [{title: '', data: []}];
+    records.forEach((record_item) => {
+      let value = results.find((results_item) => {
+        return results_item.title === record_item.key.charAt(0);
+      });
+      if (typeof value === 'undefined') {
+        let index = results.push({title: record_item.key.charAt(0), data: []});
+        results[index-1].data.push(record_item);
+      }else {
+        value.data.push(record_item);
+      }
+    });
     return (
       <View style={styles.container}>
-        <FlatList data={this.state.data} renderItem={this.renderItemHandler}/>
+        <ScrollView>
+          <SectionList sections={results} renderItem={this.renderItemHandler} renderSectionHeader={this.renderSectionsHandler} keyExtractor={(item, index) => index}/>
+        </ScrollView>
       </View>
     );
   }
@@ -35,6 +54,15 @@ const styles = StyleSheet.create({
   container: {
    flex: 1,
    paddingTop: 22
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
   },
   item: {
     padding: 10,
